b679362d744167fede646d346834f1eedbe82538
Jim Hester <james.f.hester@gmail.com>
2018-11-27 13:27:50 -0500

    Move the shared library into the libs folder
    This should fix issues with multiple architectures

--- src/install.libs.R
+++ src/install.libs.R
@@ -1,6 +1,7 @@
 files <- c(
   Sys.glob(paste0("*", SHLIB_EXT)),
-  Sys.glob(paste0("*", ".lib")))
+  Sys.glob(paste0("*", ".lib")),
+  Sys.glob(paste0("../rcon/*", SHLIB_EXT)))
 dest <- file.path(R_PACKAGE_DIR, paste0('libs', R_ARCH))
 dir.create(dest, recursive = TRUE, showWarnings = FALSE)
 file.copy(files, dest, overwrite = TRUE)
